6c9b7444aaa10bc9b76f55b0f624116b7f7148c3
YuaYua <johnalbertdalangin14@gmail.com>
2026-02-09 10:12:25 +0800

    Delete Practice Activity directory

--- Practice Activity/currencyConverter.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-Console.Write("Enter amount in USD: ");
-double amount = Convert.ToDouble(Console.ReadLine());
-Console.Write("Enter exchange rate from USD to EUR: ");
-double exchangeRate = Convert.ToDouble(Console.ReadLine());
-double amountInEUR = amount * exchangeRate;
-Console.Write("Amount in EUR: {0:F2} ",amountInEUR);
